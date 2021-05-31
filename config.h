6fbd0a83368d8589d99607c52f1d811a4b4c54a8
Alex Hiller <alex.hiller@morsemicro.com>
2021-05-31 21:16:49 +1000

    Found a good gap width

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int igappx    = 5;        /* size of inner gaps */
-static const unsigned int ogappx    = 5;        /* size of outer gaps */
-static const int gapsforone	    = 0;	/* 1 enable gaps when only one window is open */
+static const unsigned int igappx    = 13;        /* size of inner gaps */
+static const unsigned int ogappx    = 13;        /* size of outer gaps */
+static const int gapsforone	    = 1;	/* 1 enable gaps when only one window is open */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
